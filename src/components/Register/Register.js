8fa50e69a23ab980e1aed96bede26340c13a4528
YevtushenkoNadiia <80822732+YevtushenkoNadiia@users.noreply.github.com>
2021-06-09 00:06:30 +0300

    08.06.2021 (#1)
    * reg-form
    
    * homework
    
    Co-authored-by: Nadiia Yevtushenko <nadyyevtushenko07@gmail.com>

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,55 +1,145 @@
-import { useState } from "react";
+import './Register.scss';
+import React, { useState } from "react";
+
 
 const Register = () => {
 
-	const [reg, setReg] = useState([]);
+	const [userData, setUserData] = useState({
+			username: '',
+			email: '',
+			password: ''
+	});
 
-	// const [type, setType] = useState(
-	// 	{
-	// 		username: '',
-	// 		email: '',
-	// 		password: ''
-	// 	}
-	// )
+	const [step, setStep] = useState(1);
+	const [users, setUsers] = useState([]);
+	const [nameError, setNameError] = useState(false);
 
-	const handler = (e) => {
-		setReg(e.target.value);
-		console.log(reg)
-	};
+	const inputHandler = (e) => {
+		const { name, value } = e.target;
 
-	const handlerNext = (e) => {
-		setReg('');
-		console.log(reg)
-	};
+		setUserData((prev) =>({
+			...prev,
+			[name]: value,
+			}));
+		};
 
-const addReg = (e) => {
+const registerHandler = (e) => {
   e.preventDefault();
-	const newRegItem = {
-		username: '',
-		email: '',
-		password: ''
+	setUsers(prevState => [...prevState, userData])
+	setStep(1);
+	if(nameError){
+		setNameError(false)
 	}
-
-	console.log('newRegItem', newRegItem)
+	setUserData({
+		username: '',
+		email: '', 
+		password: '' 
+	})
 };
-	
-	return (
-		<div className="Register">
 
-			<button onClick={handlerNext}>Next step</button>
-			<form className="register__form"
-						onSubmit={addReg}>
-				<input 
-					className="register__input" 
-					onChange={handler}
-					value={reg}
-					type="text"
-					placeholder="Enter your name"
-				/>
-				<button type="submit">Register</button>
-			</form>
-		</div>
-	);
+const stepHandler = () => {
+	const user = users.find(user => user.username === userData.username)
+
+	if (user) {
+		return setNameError(true)
+	}
+	setStep(step + 1);
+	// setStep((prev) => prev + 1);
+};
+
+console.log('users', users)
+
+return (
+		<form className="register"
+					onSubmit={registerHandler}>
+						{step === 1 ? (
+						<input 
+								className={`register__input ${nameError ? 'error' : ''}`}
+								onChange={inputHandler}
+								name="username"
+								value={userData.username}
+								type="text"
+								placeholder="Enter your name"
+						/>
+						 ) : step === 2 ? (
+							<input 
+								className="register__input" 
+								onChange={inputHandler}
+								name="email"
+								value={userData.email}
+								type="text"
+								placeholder="Enter your email"
+						/>
+						) : (
+							<input 
+								className="register__input" 
+								onChange={inputHandler}
+								name="password"
+								value={userData.password}
+								type="password"
+								placeholder="Enter your password"
+							/>
+						)}
+			{step < 3
+				? <div className="register__add" style={{
+					border: '1px solid red',
+					padding: '5px 20px',
+					marginTop: 20,
+					cursor: 'pointer'
+				}}
+					   onClick={stepHandler}>Next step</div> : <button type="submit">Register</button>}
+		</form>
+);
+
+	// return (
+	// 	<div className="register">
+	// 		{step < 3 ? <button onClick={stepHandler}>Next step</button> : null}
+	// 		<form className="register__form"
+	// 					onSubmit={registerHandler}>
+	// 						{step === 1 ? (
+	// 						<input 
+	// 								className="register__input" 
+	// 								onChange={inputHandler}
+	// 								name="username"
+	// 								value={userData.username}
+	// 								type="text"
+	// 								placeholder="Enter your name"
+	// 						/>
+	// 						 ) : step === 2 ? (
+	// 							<input 
+	// 								className="register__input" 
+	// 								onChange={inputHandler}
+	// 								name="email"
+	// 								value={userData.email}
+	// 								type="text"
+	// 								placeholder="Enter your email"
+	// 						/>
+	// 						) : (
+	// 							<input 
+	// 								className="register__input" 
+	// 								onChange={inputHandler}
+	// 								name="password"
+	// 								value={userData.password}
+	// 								type="password"
+	// 								placeholder="Enter your password"
+	// 							/>
+	// 						)}
+				
+	// 			<button type="submit">Register</button>
+	// 		</form>
+	// 	</div>
+	// );
+
+
+
 };
 
 export default Register;
+
+
+
+
+
+
+
+
