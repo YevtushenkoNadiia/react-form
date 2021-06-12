9e8c2184ab7bfccf9287d3de7464762da15ddde2
YevtushenkoNadiia <80822732+YevtushenkoNadiia@users.noreply.github.com>
2021-06-12 11:14:23 +0300

    10.06.2021 (#2)
    * reg-form
    
    * lesson 30
    
    Co-authored-by: Nadiia Yevtushenko <nadyyevtushenko07@gmail.com>

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,137 +1,73 @@
-import './Register.scss';
-import React, { useState } from "react";
-
+import { useState } from "react";
 
 const Register = () => {
 
-	const [userData, setUserData] = useState({
+	const [reg, setReg] = useState({
 			username: '',
 			email: '',
 			password: ''
 	});
 
 	const [step, setStep] = useState(1);
-	const [users, setUsers] = useState([]);
-	const [nameError, setNameError] = useState(false);
 
 	const inputHandler = (e) => {
 		const { name, value } = e.target;
-
-		setUserData((prev) =>({
+		setReg((prev) =>({
 			...prev,
 			[name]: value,
 			}));
 		};
 
-const registerHandler = (e) => {
+	const stepHandler = () => {
+		setStep((prev) => prev + 1);
+		// setStep(step + 1);
+	};
+
+const addReg = (e) => {
   e.preventDefault();
-	setUsers(prevState => [...prevState, userData])
+	console.log('reg', reg);
+	setReg({username: '', email: '', password: '' });
 	setStep(1);
-	if(nameError){
-		setNameError(false)
-	}
-	setUserData({
-		username: '',
-		email: '', 
-		password: '' 
-	})
 };
-
-const stepHandler = () => {
-	const user = users.find(user => user.username === userData.username)
-
-	if (user) {
-		return setNameError(true)
-	}
-	setStep(step + 1);
-	// setStep((prev) => prev + 1);
-};
-
-console.log('users', users)
-
-return (
-		<form className="register"
-					onSubmit={registerHandler}>
-						{step === 1 ? (
-						<input 
-								className={`register__input ${nameError ? 'error' : ''}`}
-								onChange={inputHandler}
-								name="username"
-								value={userData.username}
-								type="text"
-								placeholder="Enter your name"
-						/>
-						 ) : step === 2 ? (
+	
+	return (
+		<div className="Register">
+			{step < 3 ? <button onClick={stepHandler}>Next step</button> : null}
+			<form className="register__form"
+						onSubmit={addReg}>
+							{step === 1 ? (
 							<input 
-								className="register__input" 
-								onChange={inputHandler}
-								name="email"
-								value={userData.email}
-								type="text"
-								placeholder="Enter your email"
-						/>
-						) : (
-							<input 
-								className="register__input" 
-								onChange={inputHandler}
-								name="password"
-								value={userData.password}
-								type="password"
-								placeholder="Enter your password"
+									className="register__input" 
+									onChange={inputHandler}
+									name="username"
+									value={reg.username}
+									type="text"
+									placeholder="Enter your name"
 							/>
-						)}
-			{step < 3
-				? <div className="register__add" style={{
-					border: '1px solid red',
-					padding: '5px 20px',
-					marginTop: 20,
-					cursor: 'pointer'
-				}}
-					   onClick={stepHandler}>Next step</div> : <button type="submit">Register</button>}
-		</form>
-);
-
-	// return (
-	// 	<div className="register">
-	// 		{step < 3 ? <button onClick={stepHandler}>Next step</button> : null}
-	// 		<form className="register__form"
-	// 					onSubmit={registerHandler}>
-	// 						{step === 1 ? (
-	// 						<input 
-	// 								className="register__input" 
-	// 								onChange={inputHandler}
-	// 								name="username"
-	// 								value={userData.username}
-	// 								type="text"
-	// 								placeholder="Enter your name"
-	// 						/>
-	// 						 ) : step === 2 ? (
-	// 							<input 
-	// 								className="register__input" 
-	// 								onChange={inputHandler}
-	// 								name="email"
-	// 								value={userData.email}
-	// 								type="text"
-	// 								placeholder="Enter your email"
-	// 						/>
-	// 						) : (
-	// 							<input 
-	// 								className="register__input" 
-	// 								onChange={inputHandler}
-	// 								name="password"
-	// 								value={userData.password}
-	// 								type="password"
-	// 								placeholder="Enter your password"
-	// 							/>
-	// 						)}
+							 ) : step === 2 ? (
+								<input 
+									className="register__input" 
+									onChange={inputHandler}
+									name="email"
+									value={reg.email}
+									type="text"
+									placeholder="Enter your email"
+							/>
+							) : (
+								<input 
+									className="register__input" 
+									onChange={inputHandler}
+									name="password"
+									value={reg.password}
+									type="password"
+									placeholder="Enter your password"
+								/>
+							)}
 				
-	// 			<button type="submit">Register</button>
-	// 		</form>
-	// 	</div>
-	// );
-
-
-
+				<button type="submit">Register</button>
+			</form>
+		</div>
+	);
 };
 
 export default Register;
