b983e2afc3dff85da70cd9fc6020f2ff7139478b
Nadiia Yevtushenko <nadyyevtushenko07@gmail.com>
2021-06-12 11:12:16 +0300

    lesson 30

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,57 +1,173 @@
+import "./Todos.scss";
+import {
+	Button,
+	Checkbox,
+	Switch,
+	FormControl,
+	InputLabel, MenuItem, Select,
+	TextField,
+} from "@material-ui/core";
+import { Delete, Edit } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import { STATUSES } from "../../constants";
 
-import './Todos.scss';
-import { useState } from "react";
 
 const Todos = () => {
+	const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+	const [newTodo, setNewTodo] = useState('');
 
-const [todos, setTodos] = useState([]);
+	const inputChange = (e) => {
+		setNewTodo(e.target.value);
+	};
 
-const [newTodo, setNewTodo] = useState('');
+	const addTodo = (e) => {
+		e.preventDefault();
+		const newTodoItem = {
+			id: Date.now(),
+			text: newTodo,
+			status: 'new',
+		}
 
-const inputChange = (e) => {
-	setNewTodo(e.target.value);
-};
+		setTodos((prevState) => [newTodoItem, ...prevState])
+		setNewTodo('')
 
-const addTodo = (e) => {
-  e.preventDefault();
-	const newTodoItem = {
-		id: Date.now(),
-		text: newTodo,
-		status: 'new'
+	};
+
+	const editTodo = id => {
+
 	}
 
-	setTodos((prevState) => [newTodoItem, ...prevState])
-	setNewTodo('')
-};
+	function statusHandler (id) {
+		const newTodos = todos.map(todo => {
+			if(todo.id === id) {
+				if (todo.status !== 'done'){
+					return {...todo, status:'done'}
+				}
+				return {...todo, status:'new'}
+			}
+			return todo
+		})
+		setTodos(newTodos)
+	}
+
+	function changeStatus (e, id) {
+		const newTodos = todos.map(todo => {
+			if (todo.id === id) {
+				return {...todo, status: e.target.value}
+			}
+			return todo
+		})
+
+		setTodos(newTodos)
+	}
+
+	function deleteTodo (id) {
+		setTodos(todos.filter(todo=>todo.id !== id))
+	}
+
+	function saveTodos () {
+		localStorage.setItem('todos', JSON.stringify(todos))
+	}
+
+	useEffect(()=>{
+		console.log('useEffect')
+
+		return ()=>{
+			// ...
+		}
+	},[todos])
 
 	return (
 		<div className="todos">
-		<form className="todos__form" 
-					onSubmit={addTodo}>
-			<input 
-				type="text" 
-				value={newTodo}
-			 	onChange={inputChange}
-			  placeholder="Your new todo..." 
-			/>
-			<button type="submit">Add todo</button>
-		</form>
-		<div className="todos__list">
-			{todos.length 
-				? (todos.map(({ id, text:other }) => {
-					return (
-						<div className="todos__item" key={id}>
-						{other}
-						</div>
-					);
-				})) 
-				: <h2>No todos...</h2>}
-		</div>
+			<form className="todos__form"
+				  onSubmit={addTodo}>
+				<TextField label="Your new todo..."
+						   type="text"
+						   size="small"
+						   name="todo"
+						   value={newTodo}
+						   onChange={inputChange}
+						   variant="outlined" />
+				<Button variant="contained"
+						type="submit"
+						color="primary">Add todo</Button>
+			</form>
+			{todos.length ? <Button variant="outlined"
+									color="primary"
+									className="todos__save"
+									onClick={saveTodos}>Save todos</Button> : null}
+			<div className="todos__autosave">
+				<Switch
+					color="primary"
+					checked={true}
+					onChange={() => statusHandler()}
+				/>
+				Autosave
+			</div>
+			<div className="todos__list">
+				{todos.length
+					? (todos.map(({id, text, status}) => {
+						return (
+							<div className="todos__item"
+								 key={id}>
+								<Checkbox
+									color='defaults'
+									checked={status === 'done'}
+									onChange={() => statusHandler(id)}
+								/>
+								<p className="todos__text">{text}</p>
+								{/*{isEditorActive ? (<TextField value={text}/> : <p className='todos__text'>{text}</p>)}*/}
+								<div className="todos__actions">
+									<FormControl className="todos__select">
+										<InputLabel>Status</InputLabel>
+										<Select
+											value={status}
+											onChange={function (e) {
+												return changeStatus(e,id)
+											}}
+										>
+											{STATUSES.map(status => (
+												<MenuItem value={status} key={status}>{status}</MenuItem>
+												))}
+										</Select>
+									</FormControl>
+									<Button startIcon={<Edit />}
+											variant={"contained"}
+											color={"primary"}
+											size="small"
+											onClick={() => editTodo(id)}>Edit</Button>
+									<Button startIcon={<Delete />}
+											variant={"contained"}
+											color={"secondary"}
+											size="small"
+											onClick={() => deleteTodo(id)}>Delete</Button>
+
+								</div>
+							</div>
+						);
+					}))
+					: <h2>No todos...</h2>}
+			</div>
 		</div>
 	);
 };
 
 export default Todos;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // import './Todos.scss';
 // import { useState } from "react";
