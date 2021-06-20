949d1ac1bdcf0c34412bcc45c07cb86af5f2c50d
Nady <nadyevtushenko07@gmail.com>
2021-06-20 11:15:25 +0300

    lesson 32

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -5,9 +5,9 @@ import {
 	TextField,
 } from "@material-ui/core";
 import { useEffect, useState } from "react";
+import { STATUSES } from "../../constants";
 import IOSSwitch from "../IOSSwitch/IOSSwitch";
 import TodoItem from "../TodoItem/TodoItem";
-
 
 const Todos = () => {
 	const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
@@ -41,52 +41,82 @@ const Todos = () => {
 
 	useEffect(() => {
 		if (switchTodo) {
-			saveTodos()
+			saveTodos();
 		}
-	}, [todos])
+	}, [todos]); // eslint-disable-line react-hooks/exhaustive-deps
+
+	const [currentStatus, setCurrentStatus] = useState("new");
+
+	function tabHandler(status) {
+		setCurrentStatus(status);
+		// про синхронность setState
+		// Logger.info("tabHandler", currentStatus);
+	}
+
+	useEffect(() => {
+		// Logger.info("useEffect", currentStatus);
+	}, [currentStatus]);
+
+	useEffect(() => {
+		localStorage.setItem("switchTodo", switchTodo);
+	}, [switchTodo]);
 
 	return (
 		<div className="todos">
-			<form className="todos__form"
-				  onSubmit={addTodo}>
-				<TextField label="Your new todo..."
-						   type="text"
-						   size="small"
-						   name="todo"
-						   value={newTodo}
-						   onChange={inputChange}
-						   variant="outlined" />
-				<Button variant="contained"
-						type="submit"
-						color="primary">Add todo</Button>
+			<form className="todos__form" onSubmit={addTodo}>
+				<TextField
+					label="Your new todo..."
+					type="text"
+					size="small"
+					name="todo"
+					value={newTodo}
+					onChange={inputChange}
+					variant="outlined"
+				/>
+				<Button variant="contained" type="submit" color="primary">
+					Add todo
+				</Button>
 			</form>
-			{todos.length ? <Button variant="outlined"
-									color="primary"
-									className="todos__save"
-									onClick={saveTodos}>Save todos</Button> : null}
+			{todos.length ? (
+				<Button variant="outlined" color="primary" className="todos__save" onClick={saveTodos}>
+					Save todos
+				</Button>
+			) : null}
 
 			<div className="todos__autosave">
 				<FormControlLabel
-					control={<IOSSwitch checked={switchTodo}
-										onChange={switchHandler} />}
+					control={<IOSSwitch checked={switchTodo === "on"} onChange={switchHandler} />}
 					label="Autosave"
 				/>
 			</div>
 
+			<div className="todos__filter">
+				{STATUSES.map((status) => (
+					<Button
+						key={status}
+						variant={status === currentStatus ? "contained" : "outlined"}
+						color="primary"
+						onClick={() => tabHandler(status)}
+					>
+						{status}
+					</Button>
+				))}
+			</div>
+
 			<div className="todos__list">
-				{todos.length
-					? (todos.map((todo) => <TodoItem key={todo.id}
-													 todo={todo}
-													 todos={todos}
-													 setTodos={setTodos} />))
-					: <h2>No todos...</h2>}
+				{todos.length ? (
+					todos
+						.filter((todo) => todo.status === currentStatus)
+						.map((todo) => <TodoItem key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)
+				) : (
+					<h2>No todos...</h2>
+				)}
 			</div>
 		</div>
 	);
 };
 
 export default Todos;
-
 
 
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,6 @@
-export const STATUSES = ['new', 'in progress', 'done', 'reject', 'deleted']
+export const STATUSES = ["new", "in progress", "done", "reject", "deleted"];
+export const STATUSES2 = ["new", "in prasdasdasdogress", "done", "reject", "deleted"];
+
+export function f() {
+    // asdasd
+}
